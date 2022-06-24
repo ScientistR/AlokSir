56187aa0b5c8c15709f2d500c83afcee6647a487
ScientistR <103405631+ScientistR@users.noreply.github.com>
2022-06-24 10:31:36 +0530

    Add files via upload

---
+++ Employee.aspx
@@ -0,0 +1,90 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Employee.aspx.cs" Inherits="Test_31jan.Employee" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td>Name :</td>
+                    <td>
+                        <asp:TextBox ID="txtname" runat="server"></asp:TextBox></td>
+                </tr>
+
+                <tr>
+                    <td>Address :</td>
+                    <td>
+                        <asp:TextBox ID="txtaddress" runat="server"></asp:TextBox></td>
+                </tr>
+
+                <tr>
+                    <td>Age :</td>
+                    <td>
+                        <asp:TextBox ID="txtage" runat="server"></asp:TextBox></td>
+                </tr>
+
+                <tr>
+                    <td>Country :</td>
+                    <td>
+                        <asp:DropDownList ID="ddlcountry" runat="server" OnSelectedIndexChanged="ddlcountry_SelectedIndexChanged" AutoPostBack="true"></asp:DropDownList></td>
+                </tr>
+
+                <tr>
+                    <td>State :</td>
+                    <td>
+                        <asp:DropDownList ID="ddlstate" runat="server"></asp:DropDownList></td>
+                </tr>
+
+                <tr>
+                    <td></td>
+                    <td><asp:Button ID="btnsave" runat="server" Text="Save" OnClick="btnsave_Click" /></td>
+                </tr>
+
+                <tr>
+                    <td></td>
+                    <td><asp:GridView ID="grd" runat="server" OnRowCommand="grd_RowCommand" AutoGenerateColumns="false">
+                        <Columns>
+                            <asp:TemplateField HeaderText="Employee Name">
+                                <ItemTemplate>
+                                    <%#Eval("name") %>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+
+                            <asp:TemplateField HeaderText="Employee Address">
+                                <ItemTemplate>
+                                    <%#Eval("address") %>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+
+                            <asp:TemplateField HeaderText="Employee Age">
+                                <ItemTemplate>
+                                    <%#Eval("age") %>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+
+                            <asp:TemplateField HeaderText="Employee Country">
+                                <ItemTemplate>
+                                    <%#Eval("cname") %>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+
+                             <asp:TemplateField>
+                                <ItemTemplate>
+                                    <asp:LinkButton ID="btnedit" runat="server" Text="Edit" CommandName="A" CommandArgument='<%#Eval("empid") %>'></asp:LinkButton>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+
+                         
+                        </Columns>
+                        </asp:GridView></td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Employee.aspx.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Test_31jan
+{
+    public partial class Employee : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindCountry();
+                ddlstate.Enabled = false;
+                ddlstate.Items.Insert(0, new ListItem("--Select State--", "0"));
+                BindGrid();
+            }
+        }
+        public void BindCountry()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("usp_country_get", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            con.Close();
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                ddlcountry.DataValueField = "cid";
+                ddlcountry.DataTextField = "cname";
+                ddlcountry.DataSource = ds;
+                ddlcountry.DataBind();
+                ddlcountry.Items.Insert(0, new ListItem("--Select Country--", "0"));
+            }
+            else
+            {
+                ddlcountry.DataSource = null;
+                ddlcountry.DataBind();
+            }
+        }
+
+        public void BindState(string A)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("usp_state_get", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@cid",A);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            con.Close();
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                ddlstate.DataValueField = "sid";
+                ddlstate.DataTextField = "sname";
+                ddlstate.DataSource = ds;
+                ddlstate.DataBind();
+                ddlstate.Items.Insert(0, new ListItem("--Select State--", "0"));
+            }
+            else
+            {
+                ddlcountry.DataSource = null;
+                ddlcountry.DataBind();
+            }
+        }
+
+        public void BindGrid()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("usp_emp_get", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            con.Close();
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                grd.DataSource = ds;
+                grd.DataBind();
+            }
+            else
+            {
+                grd.DataSource = null;
+                grd.DataBind();
+            }
+        }
+
+        protected void btnsave_Click(object sender, EventArgs e)
+        {
+            if (btnsave.Text == "Save")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("usp_emp_insert", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@name", txtname.Text);
+                cmd.Parameters.AddWithValue("@address", txtaddress.Text);
+                cmd.Parameters.AddWithValue("@age", txtage.Text);
+                cmd.Parameters.AddWithValue("@ctr", ddlcountry.SelectedValue);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                BindGrid();
+            }
+            else
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("usp_emp_update", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@empid", ViewState["pp"]);
+                cmd.Parameters.AddWithValue("@name", txtname.Text);
+                cmd.Parameters.AddWithValue("@address", txtaddress.Text);
+                cmd.Parameters.AddWithValue("@age", txtage.Text);
+                cmd.Parameters.AddWithValue("@ctr", ddlcountry.SelectedValue);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                BindGrid();
+            }
+        }
+
+        protected void grd_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "A")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("usp_emp_edit", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@empid",e.CommandArgument);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                con.Close();
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    txtname.Text = ds.Tables[0].Rows[0]["name"].ToString();
+                    txtaddress.Text = ds.Tables[0].Rows[0]["address"].ToString();
+                    txtage.Text = ds.Tables[0].Rows[0]["age"].ToString();
+                    ddlcountry.SelectedValue = ds.Tables[0].Rows[0]["ctr"].ToString();
+                    btnsave.Text = "Update";
+                    ViewState["pp"] = e.CommandArgument;
+                }
+            }
+        }
+
+        protected void ddlcountry_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ddlstate.Enabled = true;
+            BindState(ddlcountry.SelectedValue);
+        }
+    }
+}
